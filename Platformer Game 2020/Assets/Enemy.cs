dca1be2109bbe8887176fd945a956c701cd565c3
DennisPalad <dfp8885@rit.edu>
2020-08-31 11:00:28 -1000

    Created Enemy Object

---
+++ Platformer Game 2020/Assets/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public int maxHealth = 100;
+    int currentHealth;
+
+    void Start() {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damage) {
+        currentHealth -= damage;
+
+        //set hurt animation
+
+        if (currentHealth <= 0) {
+            Die();
+        }
+    }
+
+    void Die() {
+        Debug.Log("Enemy died!");
+        //Set dying animation
+
+        //Disable enemy
+        GetComponent<Collider2D>().enabled = false;
+        this.enabled = false;
+    }
+}
--- Platformer Game 2020/Assets/Weapon.cs
+++ Platformer Game 2020/Assets/Weapon.cs
@@ -21,6 +21,10 @@ public class Weapon : MonoBehaviour
         RaycastHit2D hitInfo = Physics2D.Raycast(firePoint.position, firePoint.right);
         if (hitInfo)
         {
+            Enemy enemy = hitInfo.transform.GetComponent<Enemy>();
+            if (enemy != null) {
+                enemy.TakeDamage(damage);
+            }
             line.SetPosition(0, firePoint.position);
             line.SetPosition(1, hitInfo.point);
         }
