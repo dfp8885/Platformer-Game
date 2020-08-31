3c3edf68aaf888ca7be00876e462e0ef4a45475c
DennisPalad <dfp8885@rit.edu>
2020-08-30 23:30:40 -1000

    Added animations for player movement (running and jumping)

--- Platformer Game 2020/Assets/PlayerMovement.cs
+++ Platformer Game 2020/Assets/PlayerMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController2D controller;
+    public Animator animator;
 
     public float runSpeed = 40f;
 
@@ -23,9 +24,16 @@ public class PlayerMovement : MonoBehaviour
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
         if (Input.GetButtonDown("Jump")){
             jump = true;
+            animator.SetBool("Jump", true);
         }
+    }
+
+    public void OnLand() {
+        animator.SetBool("Jump", false);
     }
 
     void FixedUpdate() {
