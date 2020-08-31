e98e5369dd50755c2c2f0ff2f876ef4634ca50e8
DennisPalad <dfp8885@rit.edu>
2020-08-31 00:50:17 -1000

    Implementedd Raycast Shooting for Player

---
+++ Platformer Game 2020/Assets/Weapon.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+
+    public Transform firePoint;
+    public int damage = 40;
+    public LineRenderer line;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1")) {
+            StartCoroutine(Shoot());
+        }
+    }
+
+    IEnumerator Shoot() {
+        RaycastHit2D hitInfo = Physics2D.Raycast(firePoint.position, firePoint.right);
+        if (hitInfo)
+        {
+            line.SetPosition(0, firePoint.position);
+            line.SetPosition(1, hitInfo.point);
+        }
+        else {
+            line.SetPosition(0, firePoint.position);
+            line.SetPosition(1, firePoint.position + firePoint.right * 100);
+        }
+
+        line.enabled = true;
+
+        yield return new WaitForSeconds(0.02f);
+
+        line.enabled = false;
+    }
+}
